1a20d78d31959646968952bec2b67f22068687df
Omega Dziko F. <megaoy@yahoo.com>
2024-11-14 21:01:01 -0500

    works well

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
+import { loadArticles } from './store/articleReducer';
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.loadArticles = loadArticles;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+
       <App />
     </Provider>
   </React.StrictMode>
